cf125e1427407038b71eee8d8483c2cfd10b6cb0
jorgeucas lucas <jorge_dofus1@hotmail.com>
2021-03-18 15:29:47 -0300

    update 180321

--- HelloWorld15/Program.cs
+++ HelloWorld15/Program.cs
@@ -10,20 +10,20 @@ namespace HelloWorld15
     {
         static void Main(string[] args)
         {
-            // double x = null; // struct não pode receber valor nulo.
+            // double x = null; // por padrão struct não pode receber valor nulo.
 
-            double? x = null; //poder usar nulo em struct. Ou Nullable<double> x = null; // útil para campos não obrigatorios (datas, números)
+            double? x = null; // é possível usar nulo com o operador nullable '?' ou 'Nullable<double> x = null'; // útil para campos não obrigatorios (datas, números).
             double? y = 10.0;
 
-            Console.WriteLine(x.GetValueOrDefault()); // método 'x.GetValueOrDefault()' obtem o valor padrão da variável, nesse caso é '0'.
+            Console.WriteLine(x.GetValueOrDefault()); // método 'x.GetValueOrDefault()' obtem o valor padrão da variável, nesse caso é '0' por ser nulo.
             Console.WriteLine(y.GetValueOrDefault());
 
-            Console.WriteLine(x.HasValue); // método 'x.HasValue' indica se tem ou não valor, 'true' ou 'false'
+            Console.WriteLine(x.HasValue); // método 'x.HasValue' indica se tem ou não valor, 'true' ou 'false'.
             Console.WriteLine(y.HasValue);
 
 
             // Operador de coalescência nula '??'
-            double a = x ?? 5; // se o 'x' valer nulo o '5' será atribuido no lugar do 'x'
+            double a = x ?? 5; // se o 'x' for null o '5' será atribuido no lugar do 'x'.
             double b = y ?? 5;
 
             Console.WriteLine();
